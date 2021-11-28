b524d76a0c9c92d86bf9ca990341c599e3921725
weih210287 <54532629+weih210287@users.noreply.github.com>
2021-11-28 20:18:33 +0800

    Delete Lesson Note/w21 directory

--- Lesson Note/w21/Gomoku/src/constants/breakpoints.js
+++
@@ -1,3 +0,0 @@
-export const MEDIA_QUERY_MD = "@media screen and (max-width: 1280px)";
-export const MEDIA_QUERY_SM = "@media screen and (max-width: 980px)";
-export const MEDIA_QUERY_XS = "@media screen and (max-width: 768px)";
